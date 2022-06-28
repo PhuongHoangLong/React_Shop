9d7ba363cba8378b210074cbffa0376b69a85726
PhuongHoangLong <phuonghoanglong2001@gmail.com>
2022-06-28 17:16:07 +0700

    4 files modified, 1 added, 1 deleted

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 const Product = () => {
   const [data, setData] = useState([]);
+  const [shop, setshop] = useState(null);
   useEffect(() => {
     console.log('app useeffect!!');
     let url = 'https://62b90e92ff109cd1dc8ad594.mockapi.io/Shop';
@@ -12,6 +14,7 @@ const Product = () => {
       .then((response) => response.json())
       .then((data) => {
         setData(data); //setStudents(data)
+   
       });
     console.log(data);
   }, []);
@@ -19,14 +22,16 @@ const Product = () => {
   return (
     <div className="container  ">
       <div className="row card-deck ">
-        {data.map((item, index) => {
+        {data.map((item) => {
           return (
             <div className="col-sm-4">
               <img className="card-img-top mt-3" src={item.images} />
               <div className="card-body">
                 <h4 className="card-title">{item.name}</h4>
                 <p className="card-text">{item.price}đ</p>
-                <button className="btn btn-primary">Buy</button>
+                <Link to={'/images/' + item.id}>
+                  <button className="btn btn-primary">Buy</button>
+                </Link>
               </div>
             </div>
           );
---
+++ src/Pages/images.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const images = () => {
+  const params = useParams();
+  const [shop, setshop] = useState(null);
+  useEffect(() => {
+    console.log('images use effect!!');
+    let url = 'https://62b90e92ff109cd1dc8ad594.mockapi.io/Shop' + params.id;
+
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.name);
+
+        setshop(data); //setshops(data)
+      });
+  }, []);
+
+  return (
+    <>
+      {shop != null ? (
+        <div class="container bootstrap snippets bootdey">
+          <div class="panel-body inf-content">
+            <div class="row">
+              <div class="col-md-6">
+                <table>
+                  <tr>
+                    <td>Name</td>
+                    <td class="text-primary">{shop.id}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Price</strong>
+                    </td>
+                    <td class="text-primary">{shop.price}</td>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <strong>Email</strong>
+                    </td>
+                    <td class="text-primary">{shop.bio}</td>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <strong>Picture</strong>
+                    </td>
+
+                    <td class="text-primary">
+                      <img src={shop.images} className="img-circle" />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <strong>Bio</strong>
+                    </td>
+                    <td>{shop.bio}</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        'loading'
+      )}
+    </>
+  );
+};
+
+export default images;
